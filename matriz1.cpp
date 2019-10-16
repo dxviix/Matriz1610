1fe7bc7c52bff27b053b9d021c2d43f0dbb6e24e
dxviix <55061508+dxviix@users.noreply.github.com>
2019-10-16 09:17:25 -0300

    Add files via upload

---
+++ matriz1.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	int MAT[3][4];
+	int i,j, cont= 0;
+
+	
+	printf("Digite valores para preencher uma matriz 3x4\n\n");
+	
+	for(i=0; i<3; i++) {
+		for(j=0; j<4; j++) {
+			scanf("%d", &MAT[i][j]);
+		}
+	}
+	
+	for(i=0; i<3; i++) {
+		for(j=0; j<4; j++) {
+			printf("%d - ", &MAT[i][j]);
+		}
+		printf("%d\n", &MAT[i][j]);
+	}
+	
+	return 0;
+}
